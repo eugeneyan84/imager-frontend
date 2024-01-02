f59a86effdee6507c0909c1e974b0563336bfe61
Eugene Yan <enegue84@gmail.com>
2024-01-02 12:22:15 +0800

    initial setup of NavLinks component, and integration into MainNav

---
+++ src/shared/components/Navigation/NavLinks.jsx
@@ -0,0 +1,25 @@
+import './NavLinks.css';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = ({}) => {
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          All Users
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/a1/places">My Places</NavLink>
+      </li>
+      <li>
+        <NavLink to="/places/new">Add New Place</NavLink>
+      </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
+    </ul>
+  );
+};
+
+export default NavLinks;
