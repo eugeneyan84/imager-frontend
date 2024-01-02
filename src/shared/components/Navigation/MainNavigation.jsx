9044b15ffeb5d799c2777751f04b5203b6832371
Eugene Yan <enegue84@gmail.com>
2024-01-02 17:48:34 +0800

    integrate backdrop into MainNav using portals again

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -1,20 +1,38 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 import MainHeader from './MainHeader';
 import './MainNavigation.css';
 import NavLinks from './NavLinks';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const handleDrawerBtnClick = () => {
+    setDrawerIsOpen((flag) => !flag);
+  };
+
+  const handleCloseDrawer = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <>
-      <SideDrawer>
-        <nav className="main-navigation__drawer-nav">
-          <NavLinks />
-        </nav>
-      </SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={handleCloseDrawer} />}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={handleDrawerBtnClick}
+        >
           <span />
           <span />
           <span />
