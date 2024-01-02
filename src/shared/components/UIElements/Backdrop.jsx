dd9d19f82fec0492158ae7fd7093ad727b17e43d
Eugene Yan <enegue84@gmail.com>
2024-01-02 17:48:14 +0800

    backdrop component with styling

---
+++ src/shared/components/UIElements/Backdrop.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom';
+
+import './Backdrop.css';
+
+const Backdrop = ({ onClick }) => {
+  return ReactDOM.createPortal(
+    <div className="backdrop" onClick={onClick}></div>,
+    document.getElementById('backdrop-container')
+  );
+};
+
+export default Backdrop;
