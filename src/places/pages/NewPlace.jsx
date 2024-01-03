33785c781fbc480e6bd17ed44e29b0e4ae4e45ea
Eugene Yan <enegue84@gmail.com>
2024-01-03 22:26:53 +0800

    integrate input into NewPlace

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,5 +1,12 @@
+import Input from '../../shared/components/FormElements/Input';
+import './NewPlace.css';
+
 const NewPlace = () => {
-  return <div>New Place here!</div>;
+  return (
+    <form className="place-form">
+      <Input type="text" label="Title" elementOption="input" />
+    </form>
+  );
 };
 
 export default NewPlace;
