5d838cdb13fdcbbb4d3a02a051ca14d6e605ea68
Eugene Yan <enegue84@gmail.com>
2024-01-05 16:58:20 +0800

    extracted test data into a separate file

---
+++ src/shared/util/Data.js
@@ -0,0 +1,43 @@
+export const TEST_PLACES = [
+  {
+    id: 29420,
+    title: 'Tokyo Tower',
+    description:
+      'The Tokyo Tower is a communications and observation tower in the Shiba-koen district of Minato, Tokyo, Japan, built in 1958.',
+    imageUrl: 'https://i.imgur.com/44PDKWt.jpg',
+    address: '4 Chome-2-8 Shibakoen, Minato City, Tokyo 105-0011, Japan',
+    location: {
+      lat: 35.65861,
+      lng: 139.74556,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 79306,
+    title: 'Metropol',
+    description:
+      'The Metropol, formerly Neues Schauspielhaus, at 5 Nollendorfplatz in the Schöneberg district of Berlin was built in 1905 as a theatre, with a separate concert hall above, in the then-fashionable Art Nouveau style.',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Metropol_Berlin.jpg/1024px-Metropol_Berlin.jpg',
+    address: 'Nollendorfpl. 5, 10777 Berlin, Germany',
+    location: {
+      lat: 52.498889,
+      lng: 13.352778,
+    },
+    creator: 'u2',
+  },
+  {
+    id: 31058,
+    title: 'Gardens by the Bay',
+    description:
+      'A nature park spanning 101 hectares in the Central Region of Singapore, adjacent to the Marina Reservoir. The park consists of three waterfront gardens: Bay South Garden, Bay East Garden and Bay Central Garden.',
+    imageUrl:
+      'https://www.gardensbythebay.com.sg/content/dam/gbb-2021/image/about-us/our-garden-story/sustainability-efforts/main/our-sustainability.jpg.transform/resize1280x720/convert1x1/image.jpg',
+    address: '18 Marina Gardens Dr, Singapore 018953',
+    location: {
+      lat: 1.28472,
+      lng: 103.865,
+    },
+    creator: 'u1',
+  },
+];
