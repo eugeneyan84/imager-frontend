4b6ff804cd0772c3fdde2a16483e0a58a707d2e0
Eugene Yan <enegue84@gmail.com>
2024-01-05 17:02:05 +0800

    created new route for UpdatePlace

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <Routes>
           <Route index element={<Users />} />
           <Route path="/places/new" element={<NewPlace />} />
+          <Route path="/places/:placeId" element={<UpdatePlace />} />
           <Route path="/:userId/places" element={<UserPlaces />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
