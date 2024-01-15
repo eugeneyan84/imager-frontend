b5bf33e0e9513337f5ee659c16bcdcdd2793e7a5
Eugene Yan <enegue84@gmail.com>
2024-01-15 21:13:16 +0800

    rectified erroneous property name

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -4,14 +4,14 @@ import Card from '../../shared/components/UIElements/Card';
 import './UserItem.css';
 
 const UserItem = ({ item }) => {
-  const { id, image, name, placeCount } = item;
+  const { id, imageUrl, name, placeCount } = item;
 
   return (
     <li className="user-item">
       <Card className="user-item__content">
         <Link to={`/${id}/places`}>
           <div className="user-item__image">
-            <Avatar image={image} alt={name} />
+            <Avatar image={imageUrl} alt={name} />
           </div>
           <div className="user-item__info">
             <h2>{name}</h2>
