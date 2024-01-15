c3f1348fe0267e2553af642d291fcfd3c63cd18d
Eugene Yan <enegue84@gmail.com>
2024-01-15 21:33:28 +0800

    integrated useApi hook into Users page

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -2,14 +2,32 @@ import { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useApi } from '../../shared/hooks/useApi';
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useApi();
+
+  //const [isLoading, setIsLoading] = useState(false);
+  //const [error, setError] = useState();
   const [users, setUsers] = useState();
 
   useEffect(() => {
-    const sendRequest = async () => {
+    const request = async () => {
+      try {
+        const response = await sendRequest(
+          `${import.meta.env.VITE_BACKEND_HOSTNAME}/api/users`
+        );
+
+        setUsers(response.users);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    request();
+  }, [sendRequest]);
+
+  /*   useEffect(() => {
+    const sendRequest2 = async () => {
       setIsLoading(true);
       try {
         const response = await fetch(
@@ -28,16 +46,16 @@ const Users = () => {
         setIsLoading(false);
       }
     };
-    sendRequest();
+    sendRequest2();
   }, []);
 
   const errorHandler = () => {
     setError(null);
-  };
+  }; */
 
   return (
     <>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner />
