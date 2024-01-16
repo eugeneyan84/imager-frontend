610c22641c3aaeae5725babc2379138743fbdc6c
Eugene Yan <enegue84@gmail.com>
2024-01-16 12:14:46 +0800

    integrated useApi into UserPlace page to retrieve places for a givn user-id

--- src/places/pages/UserPlaces.jsx
+++ src/places/pages/UserPlaces.jsx
@@ -1,14 +1,39 @@
 import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList';
 import { TEST_PLACES } from '../../shared/util/Data';
+import { useApi } from '../../shared/hooks/useApi';
+import { useEffect, useState } from 'react';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const UserPlaces = () => {
+  const [places, setPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useApi();
   const userId = useParams().userId;
-  const filteredPlaces = TEST_PLACES.filter(
-    (place) => place.creator === userId
-  );
 
-  return <PlaceList items={filteredPlaces} />;
+  useEffect(() => {
+    const getPlaces = async () => {
+      try {
+        const response = await sendRequest(`/api/places/user/${userId}`);
+        setPlaces(response.places);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getPlaces();
+  }, [sendRequest, userId]);
+
+  /*   const filteredPlaces = TEST_PLACES.filter(
+    (place) => place.creator === userId
+  ); */
+
+  return (
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && <LoadingSpinner />}
+      {!isLoading && places && <PlaceList items={places} />}
+    </>
+  );
 };
 
 export default UserPlaces;
