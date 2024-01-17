87d1ebdd41c02ff69d016213aae9d1f5f1cc4a5e
Eugene Yan <enegue84@gmail.com>
2024-01-17 17:55:20 +0800

    file selection, preview and clearing logic

--- src/shared/components/FormElements/ImageUpload.jsx
+++ src/shared/components/FormElements/ImageUpload.jsx
@@ -1,16 +1,54 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Button from './Button';
 import './ImageUpload.css';
 
-const ImageUpload = ({ id, center }) => {
+const ImageUpload = ({ id, center, onFileInput, errorText }) => {
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState();
+  const [isValid, setIsValid] = useState(false);
+
   const fileUploadRef = useRef();
+
+  useEffect(() => {
+    if (!file) {
+      return;
+    } else {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        setPreview(fileReader.result);
+      };
+      fileReader.readAsDataURL(file);
+    }
+  }, [file]);
 
   const selectImgHandler = () => {
     fileUploadRef.current.click();
   };
 
   const fileInputChangedHandler = (event) => {
-    console.log(event.target);
+    console.log('fileInputChangedHandler');
+    let targetFile;
+    let fileIsValid = isValid;
+    if (event.target.files || event.target.files.length === 1) {
+      targetFile = event.target.files[0];
+      setFile(targetFile);
+      fileIsValid = true;
+      setIsValid(true);
+    } else {
+      fileIsValid = false;
+      setIsValid(false);
+    }
+    console.log(id);
+    console.log(fileIsValid);
+    console.log(targetFile);
+    onFileInput(id, targetFile, fileIsValid);
+  };
+
+  const clearFileSelectionHandler = () => {
+    setFile(null);
+    setPreview(null);
+    setIsValid(false);
+    onFileInput(id, null, false);
   };
 
   return (
@@ -25,12 +63,23 @@ const ImageUpload = ({ id, center }) => {
       />
       <div className={`image-upload ${center && 'center'}`}>
         <div className="image-upload__preview">
-          <img src="" alt="Preview" />
+          {preview && <img src={preview} alt="Preview" />}
+          {!preview && <p>Please select an image for upload.</p>}
         </div>
-        <Button type="button" onClick={selectImgHandler}>
-          Select Image
-        </Button>
+        <div className="row">
+          <Button type="button" onClick={selectImgHandler}>
+            Select Image
+          </Button>
+          <Button
+            type="button"
+            onClick={clearFileSelectionHandler}
+            style={{ marginRight: '0px' }}
+          >
+            Clear
+          </Button>
+        </div>
       </div>
+      {!isValid && <p>{errorText}</p>}
     </div>
   );
 };
