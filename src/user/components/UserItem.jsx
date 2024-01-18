a2085d419972d71a0950d2953d39069ce4f582e1
Eugene Yan <enegue84@gmail.com>
2024-01-18 18:05:25 +0800

    updated logic to determine places-count and final url of user avatar

--- src/user/components/UserItem.jsx
+++ src/user/components/UserItem.jsx
@@ -4,17 +4,19 @@ import Card from '../../shared/components/UIElements/Card';
 import './UserItem.css';
 
 const UserItem = ({ item }) => {
-  const { id, imageUrl, name, placeCount } = item;
+  console.log(item);
+  const { id, imageUrl, name, places } = item;
+  const placeCount = places.length;
+  const finalImageUrl = imageUrl.startsWith('http')
+    ? imageUrl
+    : import.meta.env.VITE_BACKEND_HOSTNAME + '/' + imageUrl;
 
   return (
     <li className="user-item">
       <Card className="user-item__content">
         <Link to={`/${id}/places`}>
           <div className="user-item__image">
-            <Avatar
-              image={`${import.meta.env.VITE_BACKEND_HOSTNAME}/${imageUrl}`}
-              alt={name}
-            />
+            <Avatar image={finalImageUrl} alt={name} />
           </div>
           <div className="user-item__info">
             <h2>{name}</h2>
